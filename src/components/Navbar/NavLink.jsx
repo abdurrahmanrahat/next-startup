97292dc3e6e88872eca2d3842618195d23bba4bf
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-08-08 18:24:37 +0600

    project initialize & navbar design

---
+++ src/components/Navbar/NavLink.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import ClassNames from "@/Utilities/ClassNames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({
+  children,
+  href,
+  exact = false,
+  activeClassName,
+  ...props
+}) => {
+  const path = usePathname();
+  const active = exact ? path == href : path.startsWith(href);
+  const classes = ClassNames(props.className, active && activeClassName);
+
+  if (classes) {
+    props.className = classes;
+  }
+
+  return (
+    <Link href={href} {...props}>
+      {children}
+    </Link>
+  );
+};
+
+export default NavLink;
